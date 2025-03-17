48f784f9720452ad08a0102c7aa08a5492dd577b
Aslan K <zhasik.asik01@gmail.com>
2025-03-17 06:37:44 +0400

    fix tg api

--- src/modules/core/models/telegram/telegram-api.ts
+++ src/modules/core/models/telegram/telegram-api.ts
@@ -5,7 +5,7 @@ export const TelegramApi = {
   auth: {
     webApp: async (data: TelegramAuthWebAppRequest): Promise<TelegramAuthResponse> => {
       try {
-        const response = await axiosInstance.post('/auth/web-app', data);
+        const response = await axiosInstance.post('/telegram/auth/web-app', data);
         return response.data;
       } catch (error) {
         console.error('Error in Telegram WebApp authentication:', error);
