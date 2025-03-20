3ba3410a5f5d34fd21d9ba939820980dec034b2c
Aslan K <zhasik.asik01@gmail.com>
2025-03-21 04:15:29 +0500

    init packs list

--- src/modules/packs/components/packs-list/parts/row.tsx
+++ src/modules/packs/components/packs-list/parts/row.tsx
@@ -1,11 +1,11 @@
 import { SectionMessage } from '@/components';
 import { FC } from 'react';
 import Image from 'next/image';
-import { PackItem } from '../../../model';
+import { BostItem } from '@/modules/core';
 import styles from './row.module.css';
 
 interface RowProps {
-  packs: PackItem[];
+  packs: BostItem[];
 }
 
 export const Row: FC<RowProps> = ({ packs }) => {
@@ -13,7 +13,7 @@ export const Row: FC<RowProps> = ({ packs }) => {
     <div className={styles.row}>
       {packs.map(pack => (
         <div key={pack.id} className="flex flex-col items-center justify-center">
-          <Image src={pack.image} alt={pack.name} width={100} height={100} />
+          <Image src={'/packs/card-small.png'} alt={pack.name} width={100} height={100} />
           <SectionMessage size="extra-sm" color="dark">
             <p className="font-6 font-bold">{pack.name}</p>
           </SectionMessage>
