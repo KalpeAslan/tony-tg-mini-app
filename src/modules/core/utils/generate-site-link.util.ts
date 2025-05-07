0f0c2a413990eb72756a9799ed0e53da8997fe14
Aslan K <zhasik.asik01@gmail.com>
2025-05-08 02:55:59 +0500

    refactor: update generateSiteLink utility to encode URI components for accessToken and initDataRaw

--- src/modules/core/utils/generate-site-link.util.ts
+++ src/modules/core/utils/generate-site-link.util.ts
@@ -1,5 +1,7 @@
 import { appConfig } from '@/configs';
 
 export const generateSiteLink = (accessToken: string, initDataRaw: string) => {
-  return `${appConfig.siteUrl}/?accessToken=${accessToken}&initDataRaw=${initDataRaw}`;
+  return `${appConfig.siteUrl}/?accessToken=${encodeURIComponent(accessToken)}&initDataRaw=${encodeURIComponent(
+    initDataRaw
+  )}`;
 };
